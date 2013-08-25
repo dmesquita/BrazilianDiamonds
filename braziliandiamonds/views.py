873643699711a19a6fe378433846f1dd3d674fd9
dmesquita <deh.mesquita@hotmail.com>
2013-08-25 16:01:26 -0300

    Tentando pegar os marcadores do banco de dados. Não tá mostrando os objetos

--- braziliandiamonds/views.py
+++ braziliandiamonds/views.py
@@ -1,11 +1,15 @@
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.template import RequestContext, loader
 
+from diamondslocation.models import Marker, PointOfInterest
+
 def index(request):
 	template = loader.get_template('index.html')
 	context = RequestContext(request)
-	return HttpResponse(template.render(context))
+	results = Marker.objects.all()
+	"""return HttpResponse(template.render(context))"""
+	return render_to_response('index.html', {"results": results,}, context_instance=context)
