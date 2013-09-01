245a79c0a48af5059c74c6580d18e4a073199724
dmesquita <deh.mesquita@hotmail.com>
2013-09-01 18:30:46 -0300

    Mudando a variável do bd de novo.

--- braziliandiamonds/settings.py
+++ braziliandiamonds/settings.py
@@ -9,7 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {'default': dj_database_url.config()}
 
 #DATABASES = {
     #'default': {
