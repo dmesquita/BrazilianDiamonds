073f19e9913a5cbe689f3e776ed28d4c8bd64fa9
dmesquita <deh.mesquita@hotmail.com>
2013-09-01 16:32:19 -0300

    Mudando o caminho dos templates.

--- braziliandiamonds/settings.py
+++ braziliandiamonds/settings.py
@@ -115,7 +115,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/deborah/Documentos/Apps/braziliandiamonds/braziliandiamonds/templates',
+	#'/home/deborah/Documentos/Apps/braziliandiamonds/braziliandiamonds/templates',
+	os.path.join(BASE_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (    
