fab6cdd714c3c27142b20b5d0637db22139eeb43
dmesquita <deh.mesquita@hotmail.com>
2013-09-01 16:28:22 -0300

    Mudando o caminho dos arquivos estáticos.

--- braziliandiamonds/settings.py
+++ braziliandiamonds/settings.py
@@ -60,18 +60,22 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/deborah/Documentos/Apps/braziliandiamonds/braziliandiamonds/static',
+    #'/home/deborah/Documentos/Apps/braziliandiamonds/braziliandiamonds/static',
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
